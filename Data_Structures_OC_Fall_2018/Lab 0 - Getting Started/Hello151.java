07a5a8568cce30c0776efe7f9b711369c6631de9
Trevor Martin <43895813+tmartin2@users.noreply.github.com>
2019-01-01 22:22:19 -0500

    Add files via upload

---
+++ Data_Structures_OC_Fall_2018/Lab 0 - Getting Started/Hello151.java
@@ -0,0 +1,39 @@
+/**
+ * Hello CSCI 151 graders, I am writing to you from the past; currently the date
+ * is August 6th. I am completing some of the course work early so that I may
+ * explore CSCI 151 more deeply during the coming semester. My schedule this
+ * semester is packed and I believe that doing some of the work now may benefit
+ * me later on. Thank you for reading this and I hope you have a nice day and
+ * enjoyed your summer.
+ * 
+ * @author Trevor Martin
+ *
+ *         "I have adhered to the Honor Code in this assignment" - Trevor
+ *         Martin.
+ *
+ */
+
+public class Hello151 {
+
+    public static void main(String[] args)
+
+    // Series of print statements detailing introductory information for Trevor
+    // Martin.
+    {
+	System.out.println("I love Java.");
+
+	System.out.println("I would like to be called Trevor Martin, he/him/his.");
+
+	System.out.println("I believe my CSCI username is tmartin2.");
+
+	System.out.println(
+		"I am a 2nd year, Neuroscience and Computer Science double major and expect to graduate in May 2021.");
+
+	System.out.println(
+		"That professors are so willing to support and invest themselves in their students is something truly special about Oberlin.");
+
+    }
+
+}
+
+//fileZilla 
