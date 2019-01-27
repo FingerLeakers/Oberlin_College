daf4044aa1526c86104dbd02f202450bbb96f0dd
Trevor Martin <43895813+tmartin2@users.noreply.github.com>
2019-01-27 12:24:26 -0500

    Add files via upload

--- Machine_Learning_OC_Fall_2017/Assignment 1 - k Nearest Neighbor/knn.py
+++ Machine_Learning_OC_Fall_2017/Assignment 1 - k Nearest Neighbor/knn.py
@@ -18,22 +18,65 @@
 #I also must learn what a k-Nearest neighbor is
 #Additionally using number for confidence intervals
 
-
+#=======================================================================================================
+#DEPENDENCIES
+#=======================================================================================================
 import numpy
-from sys import argv
+import random
+from sys import argv 
 from pandas import read_csv, DataFrame
 from matplotlib import pyplot 
 from sklearn.neighbors import KNeighborsClassifier
-
-
-command_line_arguments = sys.argv
+#=======================================================================================================
+#Reads file, distance function, value of k, percentages of instance of training set, and the random seed
+#=======================================================================================================
 #at index 1 will be the dataset, at 2 the activation function, at 3 the value of k
+command_line_arguments = argv
+#activation_function = argv[2]
+#value_of_k = argv[3]
+#activation_function = argv[2]
+#training_percentage = argv[3]
+#random_integer_seed = argv[4]
 
-read_in_file = pandas.read_csv(sys.argv[1])
+# import random
+# random.seed(random_integer_seed)
+# shuffled = list(yourInstances)
+# random.shuffle(shuffled)
 
-dataframe = pandas.DataFrame(read_in_file)
+file = read_csv(argv[1])
+data = DataFrame(file)
 
-features = dataframe.iloc[0]
+print("Here we see the first ten row of our new dataframe")
+print(data.head(10))
+
+labels = data.iloc[0]
+
+print("Here are the labels")
+print(labels)
+
+rows_of_data = []
+for data_row in range(data.shape[0]):
+    rows_of_data.append(data.iloc[data_row])
+       
+random_seed = random.seed(argv[3])
+shuffled_data = list(rows_of_data)
+random.shuffle(shuffled_data)
+
+cut_off_index = round(len(shuffled_data)*argv[2])
+training_set = shuffled_data[:(cut_off_index+1)]
+testing_set = shuffled_data[cut_off_index+1:]
+
+#one data point is a row,I need to randomly select a percentage of rows for i in range:
+print(cut_off_index)
+print("\n",training_set)
+print("\n",testing_set)
+
+
+#data = [random.randint(0,30) for x in range(data.shape[0])]
+
+
+
+
 
 
 
