a42abf557309b7564f63dd845fabef80edfd8be4
Trevor Martin <43895813+tmartin2@users.noreply.github.com>
2019-02-10 13:41:44 -0500

    Add files via upload

---
+++ Systems_Programming_OC_Spring_2019/hw1/diamond.c
@@ -0,0 +1,12 @@
+//Author: Trevor Martin
+//Date of Completion: ?
+//Language: C
+//Homework #: 1, diamond.c
+//Class: CSCI241 | Systems Programming | Oberlin College 
+//==========================================================
+//DESCRIPTION
+//==========================================================
+//Just uploading it to Github for now boiiii
+//==========================================================
+
+#include <stdio.h>
---
+++ Systems_Programming_OC_Spring_2019/hw1/rot123.c
@@ -0,0 +1,15 @@
+//Author: Trevor Martin
+//Date of Completion: ?
+//Language: C
+//Homework #: 1, rot123.c
+//Class: CSCI241 | Systems Programming | Oberlin College 
+//==========================================================
+//DESCRIPTION
+//==========================================================
+//Encrypts a sequence of numbers
+//==========================================================
+
+#include <stdio.h>
+#include <limits.h>
+
+
