6456fb6ea3d0ca09555a2e7de72b5b88eb0e99e0
Trevor Martin <43895813+tmartin2@users.noreply.github.com>
2019-03-25 12:16:35 -0400

    Add files via upload

--- Computer_Science_OC_Spring_2018/Lab 3 - Visualizations/pyramid.py
+++ Computer_Science_OC_Spring_2018/Lab 3 - Visualizations/pyramid.py
@@ -1,31 +1,59 @@
-#Author: Trevor Martin
-#Date of Completion: 21 February 2018
-#Data of Edits: 30 December 2018
-#Language: Python 3
-#Difficulty: Easy
-
+# Author: Trevor Martin
+# Date of Completion: 21 February 2018
+# Language: Python3
+# Class: CSCI 150 | Introductory Computer Science | Oberlin College
+# Homework#: 3, pyramid.py
+#===================================================================================================
+# DESCRIPTION
+#===================================================================================================
+# This program provides an approximation of Pi using the Monte Carlo method; this method takes n darts
+# and determines what fraction of them landed within the circle. An approximate Pi value is then outputted.
+#===================================================================================================
+# DEPENDENCIES
+#===================================================================================================
+import random
 import picture
- 
-user_specified_width = int(input("Please provide a positive width for your pyramid:"))
-user_specified_height = int(input("Please provide a number of bricks for your pyramid's height:"))
+import numpy
+#===================================================================================================
 
-canvas = picture.Picture(user_specified_width,user_specified_width)
+# for random coloration
+def Random():
+    rand = numpy.random.randint(10,254)
+    return rand
+    
+def main():
+    # get the length and width
+    user_specified_width = int(input("Please provide a positive width for your pyramid:"))
+    user_specified_height = int(input("Please provide a number of bricks for your pyramid's height:"))
 
-canvas.display()
-canvas.setFillColor(0,0,0)
-canvas.drawRectFill(0,0,user_specified_width, user_specified_width)  
-canvas.setOutlineColor(0,0,0)
+    # make a canvas
+    canvas = picture.Picture(user_specified_width,user_specified_width)
 
-pyramid = (user_specified_width//user_specified_height)
+    # get random color values for the canvas
+    randX=random.randint(10,254)
+    randY=random.randint(10,254)
+    randZ=random.randint(10,254)
+     
+    # display the canvas    
+    canvas.display()
+    canvas.setFillColor(randX,randY,randZ)
+    canvas.drawRectFill(0,0,user_specified_width, user_specified_width)  
+    canvas.setOutlineColor(0,0,0)
 
-for row in range(0, user_specified_height):
-    number_of_bricks = (user_specified_height-row)
-    for column in range(0, number_of_bricks):
-        x=((pyramid//2)*row)+pyramid*(column) 
-        y=(user_specified_width-(row+1)*pyramid)
-        canvas.setFillColor(255,0,255)
-        canvas.drawRectFill(x,y, pyramid, pyramid)
-        
+    # side length of a single square 
+    pyramid_side_length = (user_specified_width//user_specified_height)
+
+    # prints each of the blocks in the pyramid
+    for row in range(user_specified_height):
+        number_of_bricks = (user_specified_height - row)
+        for column in range(number_of_bricks):
+            x = ((pyramid_side_length//2) *row) + pyramid_side_length * (column) 
+            y = (user_specified_width - (row + 1) * pyramid_side_length)
+            canvas.setFillColor(Random(), Random(), Random())
+            canvas.drawRectFill(x, y, pyramid_side_length, pyramid_side_length)
+    input()
+main()
+
 
 
       
